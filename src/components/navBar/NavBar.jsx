ef0934f263855029c2ae9d2541afdd644f437793
Casandra Marisel Elizondo <elizondo.celular@gmail.com>
2021-07-12 23:58:14 -0300

    CardWidget

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -3,18 +3,20 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import './NavBar.css';
+import CartWidget from '../cartWidget/CartWidget';
 
 function NavBar() {
     return (
         <>
-            <Navbar sticky="top" bg='dark' variant='dark'>
+            <Navbar id="nav-bar" sticky="top" bg='dark' variant='dark'>
                 <Navbar.Brand href="#">Martian</Navbar.Brand>
-                    <Nav >
+                <Nav >
                     <Nav.Link href="#">Almacen</Nav.Link>
                     <Nav.Link href="#">Verdulería</Nav.Link>
                     <Nav.Link href="#">Panadería</Nav.Link>
                     <Nav.Link href="#">Carnicería</Nav.Link>
                 </Nav>
+                <CartWidget/>
             </Navbar>
         </>
     )
