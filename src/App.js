73b053c1bc19d5d332fc9f7d6d456315076674d7
Casandra Marisel Elizondo <elizondo.celular@gmail.com>
2021-07-13 00:15:15 -0300

    ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ItemListContainer from './components/itemListContainer/ItemListContainer';
 import NavBar from './components/navBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContainer greeting="Buenos días! Disfrute de navegar en Martian"/>
     </div>
   );
 }
---
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import './ItemListContainer.css';
+
+function ItemListContainer({greeting}) {
+    return (
+        <div>
+            <h5>{greeting}</h5>
+        </div>
+    )
+}
+
+export default ItemListContainer
