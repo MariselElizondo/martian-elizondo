1a0742e8261d999d485cd4d9be16a00c88466412
Casandra Marisel Elizondo <elizondo.celular@gmail.com>
2021-07-17 12:23:20 -0300

    Contador incrementable con fecha

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,10 +1,24 @@
+import {useState} from 'react'
 import './ItemListContainer.css';
 
 function ItemListContainer({greeting}) {
+
+    const [count, setCount] = useState(0)
+    const [date, setDate] = useState(null)
+
+    const handleCount = () => {
+        setCount(count+1)
+        setDate(Date())
+    }
+
     return (
-        <div>
+        <>
             <h5>{greeting}</h5>
-        </div>
+            <h6>{count}</h6>
+            <h6>{date}</h6>
+            <button onClick={handleCount}>Subir contador</button>
+
+        </>
     )
 }
 
