3e5ddc89592d81561a84a8c691001d3cd756e3d9
Casandra Marisel Elizondo <elizondo.celular@gmail.com>
2021-07-18 01:36:13 -0300

    Clase 5: NTREGA: Contador con botón

--- src/components/itemCount/ItemCount.jsx
+++ src/components/itemCount/ItemCount.jsx
@@ -1,23 +1,24 @@
-import { useState, useEffect } from 'react'
-import { Button, Col, Card } from 'react-bootstrap'
-import './ItemCount.css';
+import { useState } from 'react'
+import { Button, Card } from 'react-bootstrap'
+import './ItemCount.css'
 
 function ItemCount({ name, initial, stock, onAdd }) {
 
     const [count, setCount] = useState(initial)
     const [itemStock, setItemStock] = useState(stock)
-
-    useEffect(() => {
-        setItemStock(stock - count)
-        setCount(initial)
-    }, [onAdd])
  
     const handleSum = () => {
         setCount(count + 1)
     }
 
-    const handleRest = () => {
+    const handleDiscount = () => {
         setCount(count - 1)
+    }
+
+    const update = () => {
+        let newStock = itemStock - count
+        setItemStock(newStock)
+        setCount(initial)
     }
 
     return (
@@ -26,19 +27,21 @@ function ItemCount({ name, initial, stock, onAdd }) {
                 <Card.Img variant="top" src="https://media.airedesantafe.com.ar/p/d6c97d0616eba6644c1d632de53e4d67/adjuntos/268/imagenes/002/057/0002057900/se-puede-decir-que-escala-macroscopica-una-habitacion-esta-vacia-cuando-se-retiran-todos-los-elementos-que-estaban-alli-embargo-la-habitacion-nunca-estara-vacia.jpg" />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
-                    <h6>Stock: {stock} unidades </h6> 
-                    <Col>
-                        <Button variant="secondary" className="btn-count" 
-                        onClick={() => count > 0 ? handleRest(itemStock): alert("No puede ser inferior a cero")}>
-                            -
-                        </Button>
-                        <span id="count">{count}</span>
-                        <Button variant="secondary" className="btn-count" 
-                        onClick={() => count < stock ? handleSum(itemStock): alert("No hay más stock")}>
-                            +
-                        </Button>  
-                        <Button variant="dark" id="add-cart"onClick={() => onAdd(count, name)}>Agregar al carrito</Button>                
-                    </Col>
+                    <h6>Stock: {itemStock} unidades </h6> 
+                    <div className="container-1">
+                        <div className="count-discount">
+                            <Button variant="secondary" className="btn-count" 
+                            onClick={() => count > 0 ? handleDiscount(itemStock): alert("No puede ser inferior a cero")}>
+                                -
+                            </Button>
+                            <span id="count">{count}</span>
+                            <Button variant="secondary" className="btn-count" 
+                            onClick={() => count < itemStock ? handleSum(itemStock): alert("No hay más stock")}>
+                                +
+                            </Button>  
+                        </div>
+                        <Button variant="dark" id="add-cart"onClick={() => itemStock > 0 ? onAdd(count, name) & update() : alert("Acción inválida, no hay más stock")}>Agregar al carrito</Button>                
+                    </div>
                 </Card.Body>
             </Card>
         </>
