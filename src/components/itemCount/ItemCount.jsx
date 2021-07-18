55f0eb9f63cba6ce4c5d6364a31d11daf1c57372
Casandra Marisel Elizondo <elizondo.celular@gmail.com>
2021-07-18 00:09:31 -0300

    Puestos en cards con su funcionalidad

--- src/components/itemCount/ItemCount.jsx
+++ src/components/itemCount/ItemCount.jsx
@@ -1,39 +1,46 @@
-import {useState} from 'react'
-import {Button, Row, Col} from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+import { Button, Col, Card } from 'react-bootstrap'
 import './ItemCount.css';
 
-function ItemCount({stock, initial, onAdd}) {
+function ItemCount({ name, initial, stock, onAdd }) {
 
-    const [count, setCount] = useState(1)
-    //const [stock, setStock] = useState(10)
+    const [count, setCount] = useState(initial)
+    const [itemStock, setItemStock] = useState(stock)
 
-    const handleCount = () => {
-        alert('El stock es ' + stock + ', inicia como siempre en ' + initial)
-        onAdd()
-        setCount(count+1)
+    useEffect(() => {
+        setItemStock(stock - count)
+        setCount(initial)
+    }, [onAdd])
+ 
+    const handleSum = () => {
+        setCount(count + 1)
     }
 
-    const agregar = () => {
-        alert("Agrego")
+    const handleRest = () => {
+        setCount(count - 1)
     }
 
     return (
         <>
-        
-            <h6>Este producto tiene {stock} unidades en stock</h6>
-            <Col>
-                <Button class="btn-count" 
-                onClick={(stock, initial, onAdd) => handleCount(stock, initial, onAdd)}>
-                    -
-                </Button>
-                <span id="count">initial</span>
-                <Button class="btn-count" 
-                onClick={(stock, initial, onAdd) => handleCount(stock, initial, onAdd)}>
-                    +
-                </Button>                  
-            </Col>
-            
-            <Button onClick={agregar}>Agregar al carrito</Button>
+            <Card id="card">
+                <Card.Img variant="top" src="https://media.airedesantafe.com.ar/p/d6c97d0616eba6644c1d632de53e4d67/adjuntos/268/imagenes/002/057/0002057900/se-puede-decir-que-escala-macroscopica-una-habitacion-esta-vacia-cuando-se-retiran-todos-los-elementos-que-estaban-alli-embargo-la-habitacion-nunca-estara-vacia.jpg" />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <h6>Stock: {stock} unidades </h6> 
+                    <Col>
+                        <Button variant="secondary" className="btn-count" 
+                        onClick={() => count > 0 ? handleRest(itemStock): alert("No puede ser inferior a cero")}>
+                            -
+                        </Button>
+                        <span id="count">{count}</span>
+                        <Button variant="secondary" className="btn-count" 
+                        onClick={() => count < stock ? handleSum(itemStock): alert("No hay más stock")}>
+                            +
+                        </Button>  
+                        <Button variant="dark" id="add-cart"onClick={() => onAdd(count, name)}>Agregar al carrito</Button>                
+                    </Col>
+                </Card.Body>
+            </Card>
         </>
     )
 }
--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,17 +1,21 @@
 import ItemCount from '../itemCount/ItemCount';
+import { Row } from 'react-bootstrap'
 import './ItemListContainer.css';
 
 function ItemListContainer({greeting}) {
 
-    const onAdd= () => {
-        alert('toco on add')
+    const handleCount= (quantity, name) => {
+        alert('Acaba de añadir al carrito ' + quantity + ' unidades de ' + name)
     }
-
     return (
-        <>
+        <div id="content" >
             <h5>{greeting}</h5>
-            <ItemCount stock={10} initial={1} onAdd={onAdd}/>
-        </>
+            <Row className="row">
+                <ItemCount name="Remera azul" initial={1} stock={10} onAdd={handleCount}/>
+                <ItemCount name="Remera roja" initial={1} stock={6} onAdd={handleCount}/>
+                <ItemCount name="Remera blanca" initial={1} stock={50} onAdd={handleCount}/>
+            </Row>
+        </div>
     )
 }
 
