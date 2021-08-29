db36f3e23857818cdf434cafbfb733f701819f83
Casandra Marisel Elizondo <elizondo.celular@gmail.com>
2021-08-29 14:20:38 -0300

    Contact (pendiente comunicate con nosotros)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -24,15 +24,17 @@ function Contact() {
         <>
             <div className="contact">
                 <span id="follow">Seguime en:</span>
-                <a className="contact-icon" href="">
+                <a className="contact-icon" href="https://www.instagram.com/marisel.elizondo/">
                     <img src={instagram} alt="instagram"></img>
                 </a>
-                <a className="contact-icon" href="">
+                <a className="contact-icon" href="https://www.linkedin.com/in/casandra-elizondo-2981b0174/">
                     <img src={linkedin} alt="instagram"></img>
                 </a>
-                <a id="contact-web-icon" href="">
+                <div className="contact-web" style={{float:'right', clear:'left'}}>
                     <img src={dialog} alt="dialog"></img>
-                </a>
+                    <span> ¿Tenés una consulta?</span><br/>
+                    <span><a href="">Contactate con nosotros</a></span>
+                </div>
             </div>      
         </>
     )
